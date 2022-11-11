17f95afeed284ad23be94d58a731c3a9d06c094a
MohamedMarawany <87308705+MohamedMarawany@users.noreply.github.com>
2022-11-11 10:24:25 +0200

    Create 1-Rectangle_area_perimeter.py
    1- Write a program that calculates the area and the perimeter of a rectangle. The program asks the user for two numbers, each number on a separate line. One of the numbers represents the length and the other represents the width.

---
+++ Task_1/1-Rectangle_area_perimeter.py
@@ -0,0 +1,6 @@
+a = int(input("Enter Length: "))
+b = int(input("Enter Width: "))
+area = a*b
+perimeter = (a+b) * 2
+print(f"The Area of the rectangle is: {area}")
+print(f"The Perimeter of the rectangle is: {perimeter}")
