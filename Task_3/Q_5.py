0172f590c78757d6bb13248c78e10e0120a38d5b
MohamedMarawany <87308705+MohamedMarawany@users.noreply.github.com>
2022-12-01 08:19:58 +0200

    Update Q_5.py
    Done

--- Task_3/Q_5.py
+++ Task_3/Q_5.py
@@ -1,10 +1,11 @@
-import sys,math
-n,m=map(int,sys.stdin.readline().split())
-l=list(map(int,sys.stdin.readline().split()))
-l=[x+1 for x in l]
-print(l)
+n,m=map(int,input().split())
+station=list(map(int,input().split()))
 ans=-100000
-for i in range(n):
-    for j in range(m):
-        ans=max(ans,abs(i-m))
+city=[i for i in range(n)]
+# print(city,station)
+for i in city:
+    out=100000000000
+    for j in station:
+        out=min(out,abs(i-j))
+    ans=max(out,ans)
 print(ans)
