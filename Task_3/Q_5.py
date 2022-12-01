6a04315e99f614671ae52abd96fe6a89476b9e81
MohamedMarawany <87308705+MohamedMarawany@users.noreply.github.com>
2022-12-01 07:49:24 +0200

    Update Q_5.py
    Done

--- Task_3/Q_5.py
+++ Task_3/Q_5.py
@@ -1,8 +1,10 @@
-n,m = map(int, input().split())
-L = list(map(int, input().split()))
-
-ans = 0
-
-for i in range(1, m):
-    ans = max(abs(L[i-1]-L[i])/2, ans)
+import sys,math
+n,m=map(int,sys.stdin.readline().split())
+l=list(map(int,sys.stdin.readline().split()))
+l=[x+1 for x in l]
+print(l)
+ans=-100000
+for i in range(n):
+    for j in range(m):
+        ans=max(ans,abs(i-m))
 print(ans)
